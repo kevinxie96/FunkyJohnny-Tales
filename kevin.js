59570c70ce79629f5d1ef88ca2f16381de039f11
kevinxie96 <kevinxie96@g.ucla.edu>
2016-01-08 22:21:47 -0800

    Personal Website
    Some Reddit writings and Poems.

---
+++ kevin.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){
+
+      $( 'div.relative' ).show(800, function(){
+             $( 'div.fix' ).slideDown(800);
+            $( 'div.aboutme' ).slideDown(800);
+      });
+   
+
+
+
+});
+
+
+$(window).load(function() { //start after HTML, images have loaded
+ 
+    var InfiniteRotator1 =
+    {
+        init: function()
+        {
+            //initial fade-in time (in milliseconds)
+            var initialFadeIn = 1000;
+            var ain = 0;
+            //interval between items (in milliseconds)
+            var itemInterval = 5000;
+ 
+            //cross-fade time (in milliseconds)
+            var fadeTime = 2500;
+            
+            //count number of items
+            var numberOfItems = $('.rotating-item').length;
+            var numberOfItemsb = $('h2 span.spacer').length;
+            //set current item
+            var currentItem = 0;
+            var currentItemb = 0;
+ 
+            //show first item
+            $('.rotating-item').eq(currentItem).fadeIn(initialFadeIn);
+            $('h2').eq(currentItem).fadeIn(initialFadeIn);
+            
+           
+            //loop through the items
+            var infiniteLoop = setInterval(function(){
+                $('.rotating-item').eq(currentItem).fadeOut(fadeTime);
+                $('h2').eq(currentItem).fadeOut(fadeTime);
+               
+                if(currentItem == numberOfItems -1){
+                    currentItem = 0;
+                }else{
+                    currentItem++;
+                }
+                if (currentItemb == numberOfItemsb -1){
+                    currentItemb = 0;
+                }
+                else {
+                    currentItemb = currentItemb +2;
+                }
+                $('.rotating-item').eq(currentItem).fadeIn(fadeTime);
+                $('h2').eq(currentItem).fadeIn(fadeTime);
+           
+            }, itemInterval);
+        }
+    };
+
+     InfiniteRotator1.init();
+      
+ 
+});
+
+$(window).load(function(){
+    $(".slide").animate({
+        left: "+=700px",
+        fontSize: "130px",
+    }, 3000, function(){
+
+        $(".disappear").show(2000);
+        $(this).css("color","rgb(0,0,51)");
+    });
+
+
+ 
+
+});
+
